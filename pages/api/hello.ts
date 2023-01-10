2af38ba0deeadcaa00bfc97049c67dd6968048fc
ryu <ghr95223@gmail.com>
2023-01-10 11:48:10 +0900

    test

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -8,5 +8,5 @@ type Data = {
 }
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  res.status(200).json({ name: 'John Doe', job: 'Developer', company: 'Slackddd' })
+  res.status(200).json({ name: 'John Doe', job: 'Developer', company: 'Slack' })
 }
